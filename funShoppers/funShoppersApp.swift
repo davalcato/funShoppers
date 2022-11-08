217010402161c67563ae914c971c078b26e40785
davalcato <davalcato@yahoo.com>
2022-11-07 16:20:31 -0800

    first commit

---
+++ funShoppers/funShoppersApp.swift
@@ -0,0 +1,17 @@
+//
+//  funShoppersApp.swift
+//  funShoppers
+//
+//  Created by Daval Cato on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct funShoppersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
